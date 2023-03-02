e55965baf0556e04c12e9bd4ac4cc991662eceea
soforl <orlova.sofia2002@gmail.com>
2023-03-02 20:23:12 +0300

    Initial Commit

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from "path";
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication> (AppModule);
+  let port = 3000;
+  if (process.env.PORT) {
+    port = Number(process.env.PORT);
+  }
+  await app.listen(port);
+
+
+  app.useStaticAssets(join(__dirname, '..', 'static'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
+
+}
+bootstrap();
